c5fe68837849b33dbabedbde80da3256c72c6fbe
niravradadiya <nr723865@dal.ca>
2022-12-02 14:53:29 -0400

    design-fixed

--- src/components/Recording.js
+++ src/components/Recording.js
@@ -1,5 +1,5 @@
 import { ExpandLess, ExpandMore, StarBorder } from '@mui/icons-material';
-import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Collapse, Divider, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from '@mui/material'
+import { Accordion, AccordionDetails, AccordionSummary, Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Collapse, Divider, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from '@mui/material'
 import Box from '@mui/material/Box';
 import { grey } from '@mui/material/colors';
 import React, { useState } from 'react'
@@ -45,10 +45,9 @@ const Recording = () => {
                         <Typography variant="body">
                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                         </Typography>
-
                     </ListItem>
                     <ListItem>
-                        <Card sx={{ maxWidth: 600, justifyContent: "center", alignItems: "center" }}>
+                        <Card sx={{ maxWidth: 600, justifyContent: "center", alignItems: "center", marginBottom:2 }}>
                             <CardActionArea>
                                 <CardMedia>
                                     <YoutubeEmbed embedId="kqtD5dpn9C8" />
@@ -57,48 +56,42 @@ const Recording = () => {
                         </Card>
 
                     </ListItem>
-                    <ListItemButton onClick={handleClick}>
-                        <ListItemText primary="Module 2: Python PyCharm" />
-                        {open ? <ExpandLess /> : <ExpandMore />}
-                    </ListItemButton>
-                    <Collapse in={open} timeout="auto" unmountOnExit>
-                        <List component="div" disablePadding>
-                            <ListItemButton sx={{ pl: 4 }}>
-                                <Card sx={{ maxWidth: 600, justifyContent: "center", alignItems: "center" }}>
-                                    <CardActionArea>
-                                        <CardMedia>
-                                            <YoutubeEmbed embedId="56bPIGf4us0" />
-                                        </CardMedia>
-                                    </CardActionArea>
-                                </Card>
-                            </ListItemButton>
-                        </List>
-                    </Collapse>
-                    <Divider variant='middle' textAlign="left" />
-                    <ListItemButton onClick={handleClickMod3}>
-                        <ListItemText primary="Module 3: Python Colab" />
-                        {openmod3 ? <ExpandLess /> : <ExpandMore />}
-                    </ListItemButton>
-                    <Collapse in={openmod3} unmountOnExit>
-                        <List component="div">
-                            <ListItemButton sx={{ pl: 4 }}>
-                                <Card sx={{ maxWidth: 600, justifyContent: "center", alignItems: "center" }}>
-                                    <CardActionArea>
-                                        <CardMedia>
-                                            <YoutubeEmbed embedId="inN8seMm7UI" />
-                                        </CardMedia>
-                                    </CardActionArea>
-                                </Card>
-                            </ListItemButton>
-                        </List>
-                    </Collapse>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMore />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                        >
+                            <Typography>Module 2: Python Pycharm</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <CardMedia>
+                                <YoutubeEmbed embedId="56bPIGf4us0" />
+                            </CardMedia>
+
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMore />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                        >
+                            <Typography>Module 3: Python Colab</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <CardMedia>
+                                <YoutubeEmbed embedId="inN8seMm7UI" />
+                            </CardMedia>
+                        </AccordionDetails>
+                    </Accordion>
                 </Grid>
                 <Grid item xs={4} md={4} direction="column" style={{ backgroundColor: "#ebf0ec", marginTop: 10, marginRight: 0, maxHeight: "100%", overflowY: true }}>
                     <ListItem>
                         <Typography variant="h5" marginLeft={2} fontWeight="bold">
                             User Playlist
                         </Typography>
-                        
+
                     </ListItem>
                     <Divider variant='middle' textAlign="left" />
                     <ListItem>
